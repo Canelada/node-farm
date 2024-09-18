49a489e7dd1946f7caedb83b80dd244c8d6c8f39
Matheus Leopardi Mello Canelada Campos <ma.canelada@gmail.com>
2024-09-18 15:23:53 -0300

    Modules

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
 const url = require('url');
-
+const replaceTemplate = require('./modules/replaceTemplate')
 //////////////////////////////////////////////////
 
 // FILES
@@ -35,28 +34,16 @@ const url = require('url');
 //////////////////////////////////////////////
 
 //SERVER
-const replaceTemplate = (temp,product) => {
-  let output = temp.replace (/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  
-  return output;
-}
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);
+  const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+  const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+  const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-const server = http.createServer((req, res) => {
+  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+  const dataObj = JSON.parse(data);
+
+  const server = http.createServer((req, res) => {
   const {query,pathname} = (url.parse(req.url, true))
   
   //Overview page
@@ -88,6 +75,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+  server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to request on port 8000');
 });
